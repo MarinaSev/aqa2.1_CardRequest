b45d7ae1a9469a3219918d52acec07a5d6362ce5
MarinaSev <pawpawsap@gmail.com>
2021-11-29 12:44:42 +0300

    Test for validation of form reamed

--- src/test/java/FormValidationTest.java
+++ src/test/java/FormValidationTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FormRequestTest {
+public class FormValidationTest {
     private WebDriver driver;
 
     @BeforeAll
